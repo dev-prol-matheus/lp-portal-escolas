c370237943f8c966aad0c49463d97bc07bd9c17e
JonathaRDS <jonatharodds@gmail.com>
2024-12-06 16:16:21 -0300

    att

--- index.php
+++ index.php
@@ -69,16 +69,201 @@
                 <span>
                     <img src="./assets/img/grafico.svg" alt="Prol Educa">
                 </span>
-                <h2>Adquira Mais Alunos</h2>
+                <h2>Painel administrativo</h2>
             </div>
             <div class="card">
                 <span>
                     <img src="./assets/img/chapeu.svg" alt="Prol Educa">
                 </span>
-                <h2>Adquira Mais Alunos</h2>
+                <h2>Multiplique sua receita</h2>
             </div>
         </section>
     </section>
+
+    <section class="product">
+        <section class="container">
+            <span class="span-title">O Produto</span>
+            <div class="product-wrapper">
+                <h1>Vídeo de apresentação do produto</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <video width="807" height="441" controls>
+                <source src="#" type="video/mp4">
+                Seu navegador não suporta o elemento de vídeo.
+            </video>
+            <button class="call-to-action-button">Call to Action Button</button>
+
+        </section>
+    </section>
+
+    <section class="benefits">
+        <section class="container">
+            <div class="left-benefits">
+                <img src="./assets/img/foto-diretora.png" alt="Prol Educa" width="648" height="755">
+            </div>
+            <div class="right-benefits">
+                <span class="span-title">Benefícios</span>
+                <div class="right-wrapper-benefits">
+                    <h1>Benefícios que os diretores terão ao utilizar o produto</h1>
+                    <div class="content-benefits">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <div class="information-benefit">
+                            <img src="./assets/img/check.svg" alt="Prol Educa" width="24" height="24">
+                            <div class="benefit-description">
+                                <p class="title-benefit">Benefício ou utilidade do produto</p>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                        </div>
+                        <div class="information-benefit">
+                            <img src="./assets/img/check.svg" alt="Prol Educa" width="24" height="24">
+                            <div class="benefit-description">
+                                <p class="title-benefit">Benefício ou utilidade do produto</p>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                        </div>
+                        <div class="information-benefit">
+                            <img src="./assets/img/check.svg" alt="Prol Educa" width="24" height="24">
+                            <div class="benefit-description">
+                                <p class="title-benefit">Benefício ou utilidade do produto</p>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="call-to-action-button">Call to Action Button</button>
+                </div>
+            </div>
+        </section>
+    </section>
+
+    <section class="pricing">
+        <section class="container">
+            <span class="span-title">Precificação</span>
+            <div class="pricing-wrapper">
+                <h1>Seção de planos ou valores</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div class="cards-pricing">
+                <div class="card">
+                    <div class="card-wrapper">
+                        <h2>Plano B</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+                    </div>
+                    <ul>
+                        <li>Benefício</li>
+                        <li>Benefício</li>
+                    </ul>
+                    <button class="call-to-action-button">Quero esse plano</button>
+                </div>
+                <div class="card card-seccondary">
+                    <div class="card-wrapper">
+                        <h2>Plano B</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+                    </div>
+                    <ul>
+                        <li>Benefício</li>
+                        <li>Benefício</li>
+                        <li>Benefício</li>
+                    </ul>
+                    <button class="call-to-action-button-seccondary"> Quero esse plano</button>
+                </div>
+                <div class="card">
+                    <div class="card-wrapper">
+                        <h2>Plano B</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+                    </div>
+                    <ul>
+                        <li>Benefício</li>
+                        <li>Benefício</li>
+                        <li>Benefício</li>
+                        <li>Benefício</li>
+                        <li>Benefício</li>
+                    </ul>
+                    <button class="call-to-action-button">Quero esse plano</button>
+                </div>
+            </div>
+        </section>
+    </section>
+
+    <section class="testimonials">
+        <section class="container">
+            <div class="testimonials-left">
+                <span class="span-title">Depoimentos</span>
+                <h1>Depoimentos dos diretores</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div class="testimonials-right">
+                
+            </div>
+        </section>
+    </section>
+
+    <section class="contact">
+        <section class="container">
+            <span class="span-title">Contato</span>
+            <h1>Formulário para contato</h1>
+            <p>Estamos disponíveis agora mesmo para te atender. Basta adicionar os dados nos campos abaixo e tirar suas dúvidas.</p>
+            <form class="form-contact" action="#" method="post">
+                <input type="text" id="nome" name="nome" placeholder="Nome" required><br><br>
+                <input type="email" id="email" name="email" placeholder="E-mail" required><br><br>
+                <input type="text" id="whatsapp" name="whatsapp" placeholder="Whatsapp" required><br><br>
+                <button class="call-to-action-button" type="submit">Entrar em Contato</button>
+            </form>
+        </section>
+    </section>
+
+    <section class="aboutus">
+        <section class="container">
+            <div class="about-left">
+                <span class="span-title">O Prol Educa</span>
+                <h1>Conheça o Prol Educa</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div class="about-right">
+                <img src="./assets/img/proleduca.png" alt="Prol Educa" width="600" height="373">
+            </div>
+        </section>
+    </section>
+
+    <section class="partners">
+        <section class="container">
+            <img src="./assets/img/ambevvoa.png" alt="Ambev VOA" width="132" height="46">
+            <img src="./assets/img/bndes.png" alt="BNDES" width="163" height="78">
+            <img src="./assets/img/estacaohack.png" alt="Estação Hack" width="162" height="38">
+            <img src="./assets/img/startupne.png" alt="Startup NE" width="132" height="75">
+            <img src="./assets/img/td_impacta.png" alt="TD Impacta" width="164" height="26">
+            <img src="./assets/img/vumborastartups.png" alt="Vum Bora Startup" width="132" height="67">
+        </section>
+    </section>
+
+    <footer>
+        <section class="container">
+            <div class="footer-left">
+                <img src="./assets/img/logo.png" alt="Prol Educa" width="231" height="92">
+                <p>Copyright 2024. Prol Educa Soluções Educacionais</p>
+            </div>
+            <div class="footer-right">
+                <div class="footer-links">
+                    <h2>Links do Footer</h2>
+                    <a href="#">Lorem Ipsum</a>
+                    <a href="#">Lorem Ipsum</a>
+                    <a href="#">Lorem Ipsum</a>
+                </div>
+                <div class="footer-links">
+                    <h2>Links do Footer</h2>
+                    <a href="#">Lorem Ipsum</a>
+                    <a href="#">Lorem Ipsum</a>
+                    <a href="#">Lorem Ipsum</a>
+                </div>
+                <div class="footer-links">
+                    <h2>Links do Footer</h2>
+                    <a href="#">Lorem Ipsum</a>
+                    <a href="#">Lorem Ipsum</a>
+                    <a href="#">Lorem Ipsum</a>
+                </div>
+                
+            </div>
+        </section>
+    </footer>
 
 </body>
 </html>
